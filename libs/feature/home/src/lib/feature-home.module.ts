b23a08e64b64e61332c87c840dedcc7f95f691cb
yonatanaltman <yonatan@yaltman.com>
2022-08-29 23:15:52 +0300

    add table.component.html

--- libs/feature/home/src/lib/feature-home.module.ts
+++ libs/feature/home/src/lib/feature-home.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home/home.component';
 import {FeatureHomeRoutingModule} from "./feature-home-routing.module";
+import {SharedUiLayoutModule} from "@golden-state-management/shared/ui-layout";
 
 @NgModule({
-  imports: [CommonModule, FeatureHomeRoutingModule],
+    imports: [CommonModule, FeatureHomeRoutingModule, SharedUiLayoutModule],
   declarations: [HomeComponent],
 })
 export class FeatureHomeModule {
--- libs/feature/home/src/lib/home/home.component.ts
+++ libs/feature/home/src/lib/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'golden-state-management-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  providers: [NgOptimizedImage]
 })
 export class HomeComponent implements OnInit {
   constructor() {}
