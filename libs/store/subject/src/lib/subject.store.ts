a9fd266be4b0927901a1fadbd9337380ed01e9ce
yonatanaltman <yonatan@yaltman.com>
2022-09-06 15:40:44 +0300

    not working

--- libs/store/subject/src/lib/subject.store.ts
+++ libs/store/subject/src/lib/subject.store.ts
@@ -4,48 +4,6 @@ import {map, Observable, ReplaySubject} from "rxjs";
 import {AppApiService} from "@golden-state-management/shared/ui-layout";
 
 @Injectable({providedIn: 'root'})
-export class TeamSubjectStore implements ITeamStore {
-  private _state?: Team;
-  private _state$ = new ReplaySubject<Team>(1);
-
-  constructor(private appApiService: AppApiService) {
-  }
-
-  private getTeamFormServer() {
-    return this.appApiService.getTeam().subscribe(team => this.update(team));
-  }
-
-  public currentState(): Team|undefined {
-    return this._state;
-  }
-
-  public update(team: Team) {
-    this._state = team;
-    this._state$.next(team);
-  }
-
-  public state$(): Observable<Team> {
-
-    if (!this._state) {
-      this.getTeamFormServer();
-    }
-    return this._state$.asObservable();
-  }
-
-  getHomeImages$(): Observable<string[]> {
-    return this.state$().pipe(map(team => team.homeImages));
-  }
-
-  getPlayer$(id: string): Observable<Player> {
-    return this.getPlayers$().pipe(map(players => players.find(player => '' + player.id === id) as Player));
-  }
-
-  getPlayers$(): Observable<Player[]> {
-    return this.state$().pipe(map(team => team.players));
-  }
-
-  public resetState(){
-    this.update({} as Team);
-  }
+export class TeamSubjectStore {
 
 }
